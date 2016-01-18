022663234ba97226ff1395e5ce79d2c02a7bd9dd
080027e1651d <mail@student.pg.gda.pl>
2016-01-19 00:43:41 +0100

    MainCommit

--- setIpScript.sh
+++ setIpScript.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 FILE=/home/root/usr/DefaultRepo/ipDatabase.txt
 SUBNET=192.156.100
+MASK=255.255.255.0
 
 for iter in $(seq 0 255);
 do
 	IP="$SUBNET.$iter"
 	if grep -Fxq "$IP" $FILE
 	then
-		echo "$iter Busy"
+		echo "$IP busy"
 	else
-		ifconfig $1 $IP netmask 255.255.255.0
+		#echo "" >> /etc/network/interfaces
+		echo "auto $1" >> /etc/network/interfaces
+		#echo "" >> /etc/network/interfaces
+		echo "iface $1 inet static" >> /etc/network/interfaces
+		#echo "" >> /etc/network/interfaces
+		echo "address $IP" >> /etc/network/interfaces
+		#echo "" >> /etc/network/interfaces
+		echo "netmask $MASK" >> /etc/network/interfaces
 		echo $IP >> $FILE
+		ifdown $1
+		ifup $1
 		break
 	fi
 done
